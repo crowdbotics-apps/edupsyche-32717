aedf10ed226a716535371b9bbcbcc6d26cd0a7e3
Crowdbotics <team@crowdbotics.com>
2022-01-06 22:06:17 +0000

    Update models

--- backend/edupsyche_32717/wsgi.py
+++ backend/edupsyche_32717/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edupsyche_32717.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edupsyche_32717.settings")
 
 application = get_wsgi_application()
